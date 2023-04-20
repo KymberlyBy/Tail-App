adc0090f0d978b6c34defe25b6eedcf4c1b08972
KymberlyBy <kymberly.byanile@live.com>
2023-04-20 04:51:47 +0000

    Removed all Views

--- main.py
+++ main.py
@@ -2,9 +2,5 @@ from tailApp import create_app
 
 app = create_app()
 
-@app.route ('/')
-def home():
-    return 'Welcome to Tail, the app that keeps your loved ones safe!'
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)  
--- tailApp/views.py
+++ tailApp/views.py
@@ -1,8 +1,2 @@
 from flask import Blueprint    
 
-views = Blueprint('views', __name__)
-
-#home
-@views.route('/')
-def mainPage():
-    return "<h1>Hello User</h1>"
